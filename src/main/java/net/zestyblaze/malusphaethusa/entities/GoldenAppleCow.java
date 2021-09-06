eca0e919d95039a2b970b32df9796096f1f0b13f
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2021-09-06 16:45:52 +0100

    Version 1.0.1
    - Added a French Translation! (Thanks to nocturni)
    - Duh duh duh! All the Cows can now be sheared to give their Apples!! Shearing them turns them into normal cows, similar to a Mooshroom
    - Rebalanced the Enchanted Golden Apple Cow's drops to make the mod more balanced
    - Updated the mod's mappings, changing to newer versions of Fabric API and Yarn
    - Updated the Mod's License to reflect the newer one I took
    - Bumped mod version up

--- src/main/java/net/zestyblaze/malusphaethusa/entities/GoldenAppleCow.java
+++ src/main/java/net/zestyblaze/malusphaethusa/entities/GoldenAppleCow.java
@@ -2,6 +2,7 @@ package net.zestyblaze.malusphaethusa.entities;
 
 import net.minecraft.client.render.entity.feature.SkinOverlayOwner;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.passive.PassiveEntity;
@@ -9,12 +10,16 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import net.zestyblaze.malusphaethusa.food.EnchantedGoldenMilk;
 import net.zestyblaze.malusphaethusa.food.GoldenMilk;
 import net.zestyblaze.malusphaethusa.init.EntityInit;
 import net.zestyblaze.malusphaethusa.items.GoldenWheat;
@@ -29,26 +34,17 @@ public class GoldenAppleCow extends AppleCow implements SkinOverlayOwner {
     public GoldenAppleCow createChild(ServerWorld serverWorld, PassiveEntity passiveEntity) {
         return EntityInit.GOLDEN_APPLE_COW.create(serverWorld);
     }
+
     @Override
     public boolean isBreedingItem(@NotNull ItemStack stack) {
         return stack.isOf(GoldenWheat.GOLDEN_WHEAT);
     }
-    @Override
-    public ActionResult interactMob(@NotNull PlayerEntity player, Hand hand) {
-        ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
-            player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
-            ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, GoldenMilk.GOLDEN_MILK.getDefaultStack());
-            player.setStackInHand(hand, itemStack2);
-            return ActionResult.success(this.world.isClient);
-        } else {
-            return super.interactMob(player, hand);
-        }
-    }
+
     @Override
     public boolean shouldRenderOverlay() {
         return true;
     }
+
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
@@ -59,5 +55,54 @@ public class GoldenAppleCow extends AppleCow implements SkinOverlayOwner {
         this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
         this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.add(7, new LookAroundGoal(this));
+    }
+
+    @Override
+    public ActionResult interactMob(@NotNull PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.isOf(Items.BUCKET) && !this.isBaby()) {
+            player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
+            ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, GoldenMilk.GOLDEN_MILK.getDefaultStack());
+            player.setStackInHand(hand, itemStack2);
+            return ActionResult.success(this.world.isClient);
+        } else if(itemStack.isOf(Items.SHEARS) && this.isShearable()){
+            this.sheared(SoundCategory.PLAYERS);
+            this.emitGameEvent(GameEvent.SHEAR, player);
+            if(!this.world.isClient()){
+                itemStack.damage(1, player, (player1) -> player1.sendToolBreakStatus(hand));
+            }
+            return ActionResult.success(this.world.isClient);
+        } else {
+            return super.interactMob(player, hand);
+        }
+    }
+
+    public void sheared(SoundCategory shearedSoundCategory){
+        this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_MOOSHROOM_SHEAR, shearedSoundCategory, 1.0F, 1.0F);
+        if(!this.world.isClient()){
+            ((ServerWorld)this.world).spawnParticles(ParticleTypes.EXPLOSION, this.getX(), this.getBodyY(0.5D), this.getZ(), 1, 0.0D, 0.0D, 0.0D, 0.0D);
+            this.discard();
+            CowEntity cowEntity = EntityType.COW.create(this.world);
+            assert cowEntity != null;
+            cowEntity.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.getYaw(), this.getPitch());
+            cowEntity.setHealth(this.getHealth());
+            cowEntity.bodyYaw = this.bodyYaw;
+            if (this.hasCustomName()) {
+                cowEntity.setCustomName(this.getCustomName());
+                cowEntity.setCustomNameVisible(this.isCustomNameVisible());
+            }
+            if (this.isPersistent()) {
+                cowEntity.setPersistent();
+            }
+            cowEntity.setInvulnerable(this.isInvulnerable());
+            this.world.spawnEntity(cowEntity);
+            for(int i = 0; i < 2; ++i) {
+                this.world.spawnEntity(new ItemEntity(this.world, this.getX(), this.getBodyY(1.0D), this.getZ(), new ItemStack(Items.GOLDEN_APPLE)));
+            }
+        }
+    }
+
+    public boolean isShearable() {
+        return this.isAlive() && !this.isBaby();
     }
 }
